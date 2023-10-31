3fc0c8adfb35ce9cb29685e9a8e1abd0b7b4aad9
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-31 12:59:49 +0300

    import the education content component and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
+import React from 'react';
+import EducationalContent from './components/EducationalContent';
 
-export default function App() {
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <header className="bg-blue-500 p-4 text-white text-center">
+        <h1 className="text-3xl font-bold">Fintech Savings Education</h1>
+      </header>
+      <EducationalContent />
+    </div>
+  );
 }
+
+export default App;
