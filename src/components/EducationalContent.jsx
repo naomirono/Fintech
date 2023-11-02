6ffffa729488e4090116fd5b1b3d7a9527166fbd
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-02 11:02:29 +0300

    Delete the const data

--- src/components/EducationalContent.jsx
+++ src/components/EducationalContent.jsx
@@ -1,59 +1,19 @@
-import React from 'react';
-import Category from './Category';
+import React from "react";
+import Category from "./Category";
+import { data } from "../mock/articles";
 
 const EducationalContent = () => {
-  const data = [
-    {
-      category: 'Savings Tips',
-      articles: [
-        {
-            title: 'Optimizing Savings Goals with PiggiePocket',
-            content: "Unlock the full potential of your savings journey with PiggiePocket. Discover how automated savings, personalized goals, and investment options can accelerate your path to financial security."
-          },
-          {
-            title: 'Exploring Investment Opportunities in PiggiePocket',
-            content: "Dive into the world of investments with PiggiePocket. Explore options like savings plans, mutual funds, and more to grow your wealth and achieve long-term financial success."
-          },
-          {
-            title:'Setting Realistic Savings Targets in PiggiePocket',
-            content:"Learn how to set achievable savings goals using PiggiePocket. Whether it's for a dream vacation, education, or an emergency fund, PiggiePocket provides the tools to track your progress and stay motivated."  
-          },
-          {
-            title:"Leveraging PiggiePocket's Smart Saving Features",
-            content: "Make the most of PiggiePocket's advanced saving features. From round-ups to automated transfers, discover how PiggiePocket can revolutionize your approach to saving."  
-          }
-      ]
-    },
-    {
-      category: 'Budgeting',
-      articles: [
-        {
-            title: 'Crafting a Kenyan Household Budget',
-            content: "Master the art of budgeting in Kenya. Allocate funds for rent, groceries, utilities, and more while ensuring a healthy portion goes towards savings and investments for a secure financial future."
-          },
-          {
-            title:"Smart Spending with PiggiePocket's Expense Tracking",
-            content: "Take control of your expenses with PiggiePocket's intuitive expense tracking feature. Monitor your spending habits, categorize expenses, and gain valuable insights to make informed budgeting decisions."  
-          },
-          {
-              title:"Strategizing for Emergencies: A Kenyan Perspective",
-              content:"Prepare for the unexpected with a tailored emergency budget for Kenyans. Learn how to allocate funds for medical emergencies, car repairs, and other unforeseen expenses to maintain financial stability."
-          },
-          {
-              title:"Balancing Leisure and Savings with PiggiePocket",
-              content:"Enjoy life while safeguarding your financial future. Learn how to allocate funds for entertainment, dining out, and leisure activities using PiggiePocket's budgeting tools, ensuring you strike the perfect balance between enjoyment and savings."
-          }
-      ]
-    }
-  ];
-
   return (
     <div className="container mx-auto p-8 ">
       {data.map((category, index) => (
-        <Category key={index} category={category.category} articles={category.articles} />
+        <Category
+          key={index}
+          category={category.category}
+          articles={category.articles}
+        />
       ))}
     </div>
   );
-}
+};
 
 export default EducationalContent;
