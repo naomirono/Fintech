0f17e5a3be57d4f570252412ab383e41c0f39e4c
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-02 11:01:36 +0300

    create js files to store sample data

---
+++ src/mock/faqs.js
@@ -0,0 +1,42 @@
+export const faqs = [
+  {
+    question: "1. How do I set up automatic savings in PiggiePocket?",
+    answer:
+      'You can set up automatic savings by navigating to the "Settings" section in the PiggiePocket app. From there, you can choose to enable automatic transfers from your linked bank account to your PiggiePocket savings account on a regular basis.',
+  },
+  {
+    question: "2. What investment options are available in PiggiePocket?",
+    answer:
+      "PiggiePocket offers a range of investment options, including savings plans, mutual funds, and fixed deposits. You can explore these options and choose the one that aligns with your financial goals and risk tolerance.",
+  },
+  {
+    question: "3. Can I create multiple savings goals in PiggiePocket?",
+    answer:
+      "Yes, PiggiePocket allows you to create multiple savings goals. Whether you're saving for a vacation, a new gadget, or an emergency fund, you can set up separate goals and track your progress towards each one.",
+  },
+  {
+    question: "4. How do I track my expenses in PiggiePocket?",
+    answer:
+      "PiggiePocket provides an intuitive expense tracking feature that allows you to categorize your expenses. Simply log your transactions, and PiggiePocket will provide insights into your spending habits, helping you make informed budgeting decisions.",
+  },
+  {
+    question: "5. Is my financial data secure with PiggiePocket?",
+    answer:
+      "Yes, PiggiePocket employs industry-standard security measures to protect your financial information. Our platform uses encryption protocols to safeguard your data and ensure a secure browsing experience.",
+  },
+  {
+    question: "6. Can I access PiggiePocket on multiple devices?",
+    answer:
+      "Absolutely! You can access your PiggiePocket account from any device with an internet connection. Whether you prefer using a smartphone, tablet, or computer, your PiggiePocket account will be synced across all platforms.",
+  },
+  {
+    question: "7. What should I do if I forget my PiggiePocket password?",
+    answer:
+      "If you forget your PiggiePocket password, simply click on the 'Forgot Password' link on the login page. You'll receive instructions on how to reset your password via the email associated with your  account.",
+  },
+  {
+    question: "8. Can I invite friends to join PiggiePocket and earn rewards?",
+    answer:
+      "Yes, PiggiePocket offers a referral program that allows you to invite friends to join the platform. When your friends sign up and start using PiggiePocket, both you and your friend may be eligible for rewards or bonuses.",
+  },
+];
